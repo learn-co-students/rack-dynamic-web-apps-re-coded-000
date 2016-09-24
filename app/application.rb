7366abf73e691f17fa11d0dbfe3ff4e47300b9ee
shereen messi <shereenmessi@hotmail.com>
2016-09-24 20:08:28 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,8 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, World You Lose"
+    resp.write "You Win"
     resp.finish
   end
 
